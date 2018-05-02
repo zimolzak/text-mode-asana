b3cdf01ec9913fe6547c68f59b01ab2e998218e5
Andrew Zimolzak (rp) <andyzimolzak@gmail.com>
2018-05-02 09:22:48 -0400

    Change around to read access token from a file.

--- client.py
+++ client.py
@@ -9,11 +9,13 @@ from six import print_
 
 # Instructions
 #
-# 1. Set your ASANA_ACCESS_TOKEN environment variable to a Personal Access Token obtained in your Asana Account Settings
+# 1. Set your ASANA_ACCESS_TOKEN environment variable to a Personal
+# Access Token obtained in your Asana Account Settings.
 #
-# This simple script asks the user to choose from their available Workspaces and then the first page (if more than a
-# single page exists) of available projects in order to create a task in that project.
-#
+# This simple script asks the user to choose from their available
+# Workspaces and then the first page (if more than a single page
+# exists) of available projects in order to create a task in that
+# project.
 
 def user_select_option(message, options):
     option_lst = list(options)
@@ -23,21 +25,14 @@ def user_select_option(message, options):
     index = int(input("Enter choice (default 0): ") or 0)
     return option_lst[index]
 
-
-if 'ASANA_ACCESS_TOKEN' in os.environ:
-    # create a client with a Personal Access Token
-    client = asana.Client.access_token(os.environ['ASANA_ACCESS_TOKEN'])
-    workspaces = client.workspaces.find_all()
-
-    workspace = user_select_option("Please choose a workspace", workspaces)
-
-    projects = client.projects.find_all({'workspace': workspace['id']})
-
-    project = user_select_option("Please choose a project", projects)
-
-    result = client.tasks.create_in_workspace(workspace['id'],
-                                              {'name': 'Learn to use Nunchucks',
-                                               'notes': 'Note: This is a test task created with the python-asana client.',
-                                               'projects': [project['id']]})
-
-    print_(json.dumps(result, indent=4))
+AAT = open('access_token.txt', 'r').read().splitlines()[0]
+client = asana.Client.access_token(AAT)
+workspaces = client.workspaces.find_all()
+workspace = user_select_option("Please choose a workspace", workspaces)
+projects = client.projects.find_all({'workspace': workspace['id']})
+project = user_select_option("Please choose a project", projects)
+result = client.tasks.create_in_workspace(workspace['id'],
+                                          {'name': 'Learn to use Nunchucks',
+                                           'notes': 'Note: This is a test task created with the python-asana client.',
+                                           'projects': [project['id']]})
+print_(json.dumps(result, indent=4))
